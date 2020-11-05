c37756c0ea7d4bd9f2bc3acb2b9193a505b4eed3
khangtran2020 <69701735+khangtran2020@users.noreply.github.com>
2020-11-05 13:58:53 -0500

    Update randomseed.py

--- Statistics/randomseed.py
+++ Statistics/randomseed.py
@@ -4,16 +4,18 @@ from random import seed
 from random import randint
 
 # seed random number generator
-seed(1)
+
 
 
 # generate some integers
 
 def seedint(a, b):
+    seed(1)
     value = randint(a, b)
     return value
 
 
 def seedfloat(a, b):
+    seed(1)
     value = random.uniform(a, b)
     return value
